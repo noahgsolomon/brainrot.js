74a66f3c7adeed0dbad52e9767581f6044de4d0b
Noah Solomon <noahsolomon2003@gmail.com>
2024-02-10 17:42:24 -0500

    queue state

--- src/app/usecreatevideo.tsx
+++ src/app/usecreatevideo.tsx
@@ -3,9 +3,13 @@ import { create } from "zustand";
 interface UseCreateVideoProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
+  isInQueue: boolean;
+  setIsInQueue: (isInQueue: boolean) => void;
 }
 
 export const useCreateVideo = create<UseCreateVideoProps>((set) => ({
   isOpen: false,
   setIsOpen: (isOpen) => set({ isOpen }),
+  setIsInQueue: (isInQueue) => set({ isInQueue }),
+  isInQueue: false,
 }));
