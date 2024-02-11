9727a0a9b694745c29fcc5ebb5a9edeb4f2ab2ac
Noah Solomon <noahsolomon2003@gmail.com>
2024-02-10 19:31:24 -0500

    your videos implementation (we're so bacK)

--- src/app/yourvideos.tsx
+++ src/app/yourvideos.tsx
@@ -1,15 +1,83 @@
 "use client";
 
 import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useAuth } from "@clerk/nextjs";
 import { useYourVideos } from "./useyourvideos";
+import { useCreateVideo } from "./usecreatevideo";
+import { trpc } from "@/trpc/client";
+import { useEffect, useState } from "react";
 
 export default function YourVideos({ visible = false }: { visible?: boolean }) {
+  const user = useAuth();
+
   const { isOpen, setIsOpen } = useYourVideos();
+  const { setIsOpen: setIsCreateVideoOpen } = useCreateVideo();
+
+  const userVideosQuery = trpc.user.userVideos.useQuery();
+
+  const [videos, setVideos] = useState(userVideosQuery.data?.videos ?? []);
+
+  useEffect(() => {
+    setVideos(userVideosQuery.data?.videos ?? []);
+  }, [userVideosQuery.data?.videos]);
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent className=" rounded-lg sm:max-w-[425px]">
-        You have no videos
+        {userVideosQuery.isFetched && videos.length > 0 ? (
+          <div className="flex flex-col items-center justify-center">
+            {videos.map((video) => (
+              <>
+                <p className="font-bold underline">{video.title}</p>
+                <p className="">
+                  <span className="font-bold">
+                    {video.agent1
+                      .split("_")
+                      .map(
+                        (word) => word.charAt(0) + word.slice(1).toLowerCase(),
+                      )
+                      .join(" ")}
+                  </span>{" "}
+                  and{" "}
+                  <span className="font-bold">
+                    {video.agent2
+                      .split("_")
+                      .map(
+                        (word) => word.charAt(0) + word.slice(1).toLowerCase(),
+                      )
+                      .join(" ")}
+                  </span>
+                </p>
+                <video
+                  className="scale-[90%] rounded-lg border shadow-md transition-all"
+                  width={300}
+                  height={300}
+                  src={video.url}
+                  loop
+                  muted
+                  controls
+                ></video>
+              </>
+            ))}
+          </div>
+        ) : (
+          <>
+            You have no videos
+            <div>
+              Click{" "}
+              <span
+                onClick={() => {
+                  setIsOpen(false);
+                  setIsCreateVideoOpen(true);
+                }}
+                className="cursor-pointer font-bold underline transition-all hover:opacity-80"
+              >
+                here
+              </span>{" "}
+              to create a video.
+            </div>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
