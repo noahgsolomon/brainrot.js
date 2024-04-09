7d4a8533b0c95a42e77ecf807f04b762489c77c9
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-09 19:45:49 -0400

    requirements

--- generate/dbClient.mjs
+++ generate/dbClient.mjs
@@ -6,14 +6,14 @@ dotenv.config();
 const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
-	database: process.env.DB_NAME,
+	uri: process.env.DB_URL,
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT || 3306,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
 	ssl: {
-		rejectUnauthorized: true,
+		rejectUnauthorized: false,
 	},
 });
 
