f66d5b3353fbb34a8160e1550f636fca02846ea6
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-10 15:40:42 -0400

    wall of fame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { trpc } from "@/trpc/client";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { Tweet } from "react-tweet";
 
 export default function Home({
   searchParams,
@@ -199,128 +200,14 @@ export default function Home({
         </Link>
       </p>
       <div className="pt-48 coarse:hidden">
-        <p className="text-center text-3xl font-bold">
-          Most Recent User Generations
-        </p>
+        <p className="text-center text-3xl font-bold">Wall of Fame</p>
         <div className="flex max-w-[1200px] flex-col items-center justify-center gap-4 md:flex-row md:flex-wrap">
-          <video
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            ref={videoRef1}
-            onMouseEnter={() => handleMouseEnter(videoRef1)}
-            onMouseLeave={() => handleMouseLeave(videoRef1)}
-            width={300}
-            height={300}
-            src={
-              "https://images.smart.wtf/videos/005e7fc7-d4aa-452e-b638-9997364570ee.mp4"
-            }
-            loop
-            controls
-            muted
-          ></video>
-          <video
-            ref={videoRef2}
-            onMouseEnter={() => handleMouseEnter(videoRef2)}
-            onMouseLeave={() => handleMouseLeave(videoRef2)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100 "
-            width={300}
-            height={300}
-            src={
-              "https://images.smart.wtf/videos/4e8982c3-d0b6-4fa4-8666-7a6b40d524f7.mp4"
-            }
-            loop
-            controls
-            muted
-          ></video>
-          <video
-            ref={videoRef3}
-            onMouseEnter={() => handleMouseEnter(videoRef3)}
-            onMouseLeave={() => handleMouseLeave(videoRef3)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={
-              "https://images.smart.wtf/videos/54f40c27-d4b8-481b-9c37-9637c8d92164.mp4"
-            }
-            loop
-            muted
-            controls
-          ></video>
-          <video
-            ref={videoRef4}
-            onMouseEnter={() => handleMouseEnter(videoRef4)}
-            onMouseLeave={() => handleMouseLeave(videoRef4)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={
-              "https://images.smart.wtf/videos/699d563a-b438-4df5-9618-db5024632055.mp4"
-            }
-            loop
-            muted
-            controls
-          ></video>
-          <video
-            ref={videoRef5}
-            onMouseEnter={() => handleMouseEnter(videoRef5)}
-            onMouseLeave={() => handleMouseLeave(videoRef5)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={
-              "https://images.smart.wtf/videos/27982a37-5197-4622-8949-6e86ce35212f.mp4"
-            }
-            loop
-            muted
-            controls
-          ></video>
-          <video
-            ref={videoRef6}
-            onMouseEnter={() => handleMouseEnter(videoRef6)}
-            onMouseLeave={() => handleMouseLeave(videoRef6)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={"https://images.smart.wtf/videos/random1.mp4"}
-            loop
-            muted
-            controls
-          ></video>
-          <video
-            ref={videoRef7}
-            onMouseEnter={() => handleMouseEnter(videoRef7)}
-            onMouseLeave={() => handleMouseLeave(videoRef7)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={"https://images.smart.wtf/videos/random2.mp4"}
-            loop
-            muted
-            controls
-          ></video>
-          <video
-            ref={videoRef8}
-            onMouseEnter={() => handleMouseEnter(videoRef8)}
-            onMouseLeave={() => handleMouseLeave(videoRef8)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={"https://images.smart.wtf/videos/random3.mp4"}
-            loop
-            muted
-            controls
-          ></video>
-          <video
-            ref={videoRef9}
-            onMouseEnter={() => handleMouseEnter(videoRef9)}
-            onMouseLeave={() => handleMouseLeave(videoRef9)}
-            className="scale-[90%] rounded-lg border opacity-50 shadow-md transition-all hover:opacity-100"
-            width={300}
-            height={300}
-            src={"https://images.smart.wtf/videos/random4.mp4"}
-            loop
-            muted
-            controls
-          ></video>
+          <Tweet id="1777773736416206948" />
+          <Tweet id="1777767131998019762" />
+          <Tweet id="1778059188742099419" />
+          <Tweet id="1777944936874393936" />
+          <Tweet id="1778012232250532213" />
+          <Tweet id="1777789848645959783" />
         </div>
       </div>
     </main>
