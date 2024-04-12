712910c5660f79e3db438d9649d82e967881a448
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-12 09:13:39 -0400

    testing new embed for videos

--- src/app/(renders)/layout.tsx
+++ src/app/(renders)/layout.tsx
@@ -1,8 +1,5 @@
 import React, { ReactNode } from "react";
 import "../globals.css";
-import NavBar from "@/components/nav/navbar";
-import { TRPCReactProvider } from "@/trpc/react";
-import { headers } from "next/headers";
 import { type Metadata } from "next";
 import { GeistSans } from "geist/font";
 import { Analytics } from "@vercel/analytics/react";
@@ -11,20 +8,8 @@ export const metadata: Metadata = {
   appleWebApp: true,
   manifest: "/manifest.json",
   icons: { apple: "/favicon.ico" },
-  openGraph: {
-    url: "https://brainrotjs.com",
-    title: "brainrotjs.com",
-    description: "Make learning fun again!",
-    images: ["https://images.smart.wtf/brainrot.png"],
-  },
   title: "brainrot.js",
   description: "Make learning fun again!",
-  twitter: {
-    card: "summary_large_image",
-    site: "brainrotjs.com",
-    creator: "@noahgsolomon",
-    images: ["https://images.smart.wtf/brainrot.png"],
-  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
