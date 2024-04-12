89c1b0c5c137ca2830cbdf58ad289340ed2a96c9
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-11 20:12:00 -0400

    no more flying gifs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,9 +101,9 @@ export default function Home({
     <main className="relative mt-6 flex flex-col items-center justify-center gap-4">
       <div className="mt-[100px] flex w-[90%] flex-col items-center justify-center bg-opacity-60 text-4xl lg:w-[80%] xl:w-[75%]">
         <div className="flex flex-col items-center justify-center gap-8 pb-8">
-          <div className="coarse:hidden">
+          {/* <div className="coarse:hidden">
             <FlyingGifs gifs={gifs} />
-          </div>
+          </div> */}
 
           <Image
             src={"https://images.smart.wtf/brainrot.png"}
