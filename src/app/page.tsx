1a4a4bf1d2cbe5e4b894a1d2c678505e6d2d3456
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-12 07:27:38 -0400

    remove github shill and new footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { useYourVideos } from "./useyourvideos";
 import { Fragment, useEffect, useMemo, useRef, useState } from "react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-import { Folder, Gem, Loader2, Star, Wand } from "lucide-react";
+import { Folder, Gem, Github, Loader2, Star, Wand } from "lucide-react";
 import { useUser } from "@clerk/nextjs";
 import { Skeleton } from "@/components/ui/skeleton";
 import { trpc } from "@/trpc/client";
@@ -16,6 +16,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Tweet } from "react-tweet";
 import FlyingGifs from "./FlyingGifs";
+import XIcon from "@/components/svg/XIcon";
 
 export default function Home({
   searchParams,
@@ -98,122 +99,119 @@ export default function Home({
   }, [isInQueue]);
 
   return (
-    <main className="relative mt-6 flex flex-col items-center justify-center gap-4">
-      <div className="mt-[100px] flex w-[90%] flex-col items-center justify-center bg-opacity-60 text-4xl lg:w-[80%] xl:w-[75%]">
-        <div className="flex flex-col items-center justify-center gap-8 pb-8">
-          {/* <div className="coarse:hidden">
+    <>
+      <main className="relative mt-6 flex flex-col items-center justify-center gap-4">
+        <div className="mt-[100px] flex w-[90%] flex-col items-center justify-center bg-opacity-60 text-4xl lg:w-[80%] xl:w-[75%]">
+          <div className="flex flex-col items-center justify-center gap-8 pb-8">
+            {/* <div className="coarse:hidden">
             <FlyingGifs gifs={gifs} />
           </div> */}
 
-          <Image
-            src={"https://images.smart.wtf/brainrot.png"}
-            width={200}
-            height={200}
-            alt="brainrot"
-            className="cursor-pointer rounded-full border-[10px] border-card shadow-lg transition-all hover:scale-[101%] active:scale-[99%] dark:border-primary"
-          />
+            <Image
+              src={"https://images.smart.wtf/brainrot.png"}
+              width={200}
+              height={200}
+              alt="brainrot"
+              className="cursor-pointer rounded-full border-[10px] border-card shadow-lg transition-all hover:scale-[101%] active:scale-[99%] dark:border-primary"
+            />
 
-          <div className=" flex flex-col items-center gap-2">
-            <Badge
-              className="cursor-pointer text-sm md:hidden"
-              variant={"hard"}
-            >
-              <Link
-                target="_blank"
-                className="flex flex-row items-center gap-2"
-                href={"https://github.com/noahgsolomon/brainrot.js"}
-              >
-                #FREE <Gem className="size-4 fill-teal-500" />
-              </Link>
-            </Badge>
-            <h1 className="relative max-w-[10ch] text-center text-5xl font-bold lg:text-6xl">
-              BRAINROT.JS
+            <div className=" flex flex-col items-center gap-2">
               <Badge
-                className="absolute -top-8 hidden cursor-pointer text-sm md:-right-12 md:block"
+                className="cursor-pointer text-sm md:hidden"
                 variant={"hard"}
               >
                 <Link
-                  className="flex flex-row items-center gap-2"
                   target="_blank"
+                  className="flex flex-row items-center gap-2"
                   href={"https://github.com/noahgsolomon/brainrot.js"}
                 >
-                  #FREE <Gem className="size-4  fill-teal-500" />
+                  #FREE <Gem className="size-4 fill-teal-500" />
                 </Link>
               </Badge>
-            </h1>
-            {pendingVideo && (
-              <div className="flex flex-row items-center gap-2 text-sm">
-                <Loader2 className="h-4 w-4 animate-spin" />
-                <div className="flex gap-2">
-                  <span className="font-bold">Place in queue:</span>{" "}
-                  {placeInQueue} <span className="font-bold">Est:</span>{" "}
-                  {(placeInQueue + 1) * 2} mins
+              <h1 className="relative max-w-[10ch] text-center text-5xl font-bold lg:text-6xl">
+                BRAINROT.JS
+                <Badge
+                  className="absolute -top-8 hidden cursor-pointer text-sm md:-right-12 md:block"
+                  variant={"hard"}
+                >
+                  <Link
+                    className="flex flex-row items-center gap-2"
+                    target="_blank"
+                    href={"https://github.com/noahgsolomon/brainrot.js"}
+                  >
+                    #FREE <Gem className="size-4  fill-teal-500" />
+                  </Link>
+                </Badge>
+              </h1>
+              {pendingVideo && (
+                <div className="flex flex-row items-center gap-2 text-sm">
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                  <div className="flex gap-2">
+                    <span className="font-bold">Place in queue:</span>{" "}
+                    {placeInQueue} <span className="font-bold">Est:</span>{" "}
+                    {(placeInQueue + 1) * 2} mins
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Button
+              className="flex flex-row items-center gap-2"
+              variant={"rainbow"}
+              disabled={pendingVideo}
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            >
+              <Wand className="h-4 w-4" /> Create Video
+            </Button>
+
+            {user.isSignedIn ? (
+              <Button
+                variant={"outline"}
+                className="flex flex-row items-center gap-2"
+                onClick={() => setIsYourVideosOpen(true)}
+              >
+                <Folder className="h-4 w-4" />
+                Your videos
+              </Button>
+            ) : !user.isLoaded ? (
+              <Skeleton className="h-[2.4rem] w-[9.3rem] rounded-lg"></Skeleton>
+            ) : null}
           </div>
         </div>
-        <div className="flex flex-col gap-2">
-          <Link
-            href={"https://github.com/noahgsolomon/brainrot.js"}
-            className={cn(buttonVariants(), "flex flex-row items-center gap-2")}
-            target="_blank"
-          >
-            <Star className="h-4 w-4 fill-yellow-500 text-yellow-300 dark:text-yellow-700" />{" "}
-            Star on GitHub
-          </Link>
-          <Button
-            className="flex flex-row items-center gap-2"
-            variant={"rainbow"}
-            disabled={pendingVideo}
-            onClick={() => {
-              setIsOpen(true);
-            }}
-          >
-            <Wand className="h-4 w-4" /> Create Video
-          </Button>
-
-          {user.isSignedIn ? (
-            <Button
-              variant={"outline"}
-              className="flex flex-row items-center gap-2"
-              onClick={() => setIsYourVideosOpen(true)}
-            >
-              <Folder className="h-4 w-4" />
-              Your videos
-            </Button>
-          ) : !user.isLoaded ? (
-            <Skeleton className="h-[2.4rem] w-[9.3rem] rounded-lg"></Skeleton>
-          ) : null}
+        <div className="pt-48">
+          <p className="text-center text-3xl font-bold">Wall of Fame</p>
+          <div className="mx-auto flex max-w-[80%] flex-col items-center justify-center gap-4 md:max-w-[1200px] md:flex-row md:flex-wrap">
+            <Tweet id="1777773736416206948" />
+            <Tweet id="1777767131998019762" />
+            <Tweet id="1778059188742099419" />
+            <Tweet id="1777944936874393936" />
+            <Tweet id="1778012232250532213" />
+            <Tweet id="1777789848645959783" />
+          </div>
         </div>
-      </div>
-      <p className="max-w-[30ch] text-sm italic">
-        <Link
-          href={"https://github.com/noahgsolomon/brainrot.js"}
-          className="font-bold"
-        >
-          Starring on GitHub
-        </Link>{" "}
-        greatly supports my efforts (this kinda costs a lot 💀) -{" "}
-        <Link
-          className="underline"
-          target="_blank"
-          href={"https://twitter.com/noahgsolomon"}
-        >
-          @Noah
-        </Link>
-      </p>
-      <div className="pt-48">
-        <p className="text-center text-3xl font-bold">Wall of Fame</p>
-        <div className="mx-auto flex max-w-[80%] flex-col items-center justify-center gap-4 md:max-w-[1200px] md:flex-row md:flex-wrap">
-          <Tweet id="1777773736416206948" />
-          <Tweet id="1777767131998019762" />
-          <Tweet id="1778059188742099419" />
-          <Tweet id="1777944936874393936" />
-          <Tweet id="1778012232250532213" />
-          <Tweet id="1777789848645959783" />
+      </main>
+      <footer className="flex w-screen justify-center border-t border-border bg-secondary px-4 py-4">
+        <div className="flex w-full items-center justify-between px-[5%] py-1 md:px-[10%]">
+          <Image
+            src={"https://images.smart.wtf/brainrot.png"}
+            width={64}
+            height={64}
+            alt="brainrot"
+            className="cursor-pointer rounded-full border border-card shadow-lg transition-all hover:scale-[101%] active:scale-[99%] dark:border-primary"
+          />
+          <div className="flex flex-row items-center justify-center gap-2">
+            <Link href={"https://github.com/noahgsolomon/brainrot.js"}>
+              <Github className="size-6" />
+            </Link>
+            <Link href={"https://twitter.com/brainrotjs"}>
+              <XIcon className="size-6 fill-primary" />
+            </Link>
+          </div>
         </div>
-      </div>
-    </main>
+      </footer>
+    </>
   );
 }
---
+++ src/components/svg/XIcon.tsx
@@ -0,0 +1,12 @@
+export default function XIcon({ className }: { className: string }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 396 396"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M301.026 37.125H355.608L236.362 173.415L376.645 358.875H266.805L180.774 246.395L82.335 358.875H27.72L155.265 213.098L20.691 37.125H133.32L211.084 139.937L301.026 37.125ZM281.869 326.205H312.114L116.886 68.079H84.4305L281.869 326.205Z" />
+    </svg>
+  );
+}
