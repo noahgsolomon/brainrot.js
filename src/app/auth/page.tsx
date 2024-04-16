19034f7206df2b93baa385145adf36bc23c9bdbb
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-16 14:56:57 -0400

    weird...... client side route

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
 import { api } from "@/trpc/server";
 import { Loader2 } from "lucide-react";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
-const Page = async () => {
-  await api.user.exists.mutate();
-  redirect("/?loggedIn=true");
+const Page = () => {
+  const existsMutation = trpc.user.exists.useMutation();
+  useEffect(() => {
+    const mutation = () => {
+      existsMutation.mutate();
+      redirect("/?loggedIn=true");
+    };
+    mutation();
+  }, [existsMutation]);
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
