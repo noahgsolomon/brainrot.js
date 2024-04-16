2e3ec3301d856ba51e795524841e7960253ad213
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-16 05:19:13 -0400

    text area default changes

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] w-full rounded-md border px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[60px] w-full rounded-lg border bg-secondary/90 px-3 py-2 text-sm shadow-sm outline-none transition-all placeholder:text-muted-foreground focus:border-lightBlue disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
