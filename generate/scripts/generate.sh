41f3a224dbf60bc6a897d4135f9c85c8d59774b6
Noah Solomon <noahsolomon2003@gmail.com>
2025-02-16 10:23:19 -0500

    working production mode

--- generate/scripts/generate.sh
+++ generate/scripts/generate.sh
@@ -11,6 +11,8 @@ fi
 if [ "$MODE" = "production" ]; then
     echo "Running production build script..."
     /root/.bun/bin/pm2 start --interpreter /root/.bun/bin/bun build.ts --name build-process --log /app/brainrot/pm2.log
+    
+    tail -f /app/brainrot/pm2.log /app/brainrot/access.log /app/brainrot/error.log
 else
     echo "Running local build script..."
     bun run /app/brainrot/localBuild.ts
--- generate/scripts/start.sh
+++ generate/scripts/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d --name brainrot-container brainrot
+docker rm -f brainrot-container 2>/dev/null || true
+
+docker run -d --name brainrot-container -e MODE=${MODE:-dev} brainrot
