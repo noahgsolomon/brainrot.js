03473b8b19021e433e2d0405182cc2ecde0afe03
Noah Solomon <noahsolomon2003@gmail.com>
2025-02-15 23:42:54 -0500

    scripts and updated dockerfile

---
+++ generate/scripts/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting Gunicorn server..."
+gunicorn --timeout 300 -w 1 -b 0.0.0.0:5005 --access-logfile access.log --error-logfile error.log --chdir /app/brainrot "transcribe:app" &
+
+# Check if MODE is set, default to development if not
+if [ -z "$MODE" ]; then
+    MODE="development"
+fi
+
+if [ "$MODE" = "production" ]; then
+    echo "Running production build script..."
+    /root/.bun/bin/pm2 start --interpreter /root/.bun/bin/bun build.ts --name build-process --log /app/brainrot/pm2.log
+else
+    echo "Running local build script..."
+    bun run /app/brainrot/localBuild.ts
+fi
---
+++ generate/scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d --name brainrot-container brainrot
