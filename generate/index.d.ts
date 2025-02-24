000be1af6e7f406b932e329e545e9666a87c5796
Noah Solomon <noahsolomon2003@gmail.com>
2025-02-24 12:57:11 -0500

    Rapper type

--- generate/index.d.ts
+++ generate/index.d.ts
@@ -1,9 +1,11 @@
 interface Transcript {
 	agentId: string;
 	text: string;
-	asset: string;
+	asset?: string;
 }
 
 type Music = 'wii' | 'none' | 'duck' | 'monkey';
 
 type VideoMode = 'brainrot' | 'podcast' | 'monologue' | 'rap';
+
+type Rapper = 'SPONGEBOB' | 'PATRICK_STAR';
