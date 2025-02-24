521ed4192a87a2727991798060c785c7241f0efa
Noah Solomon <noahsolomon2003@gmail.com>
2025-02-24 15:44:53 -0500

    new workflow

--- generate/scripts/start_prod.sh
+++ generate/scripts/start_prod.sh
@@ -1,8 +1,8 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
 
 # Stop and remove all containers
-docker stop $(docker ps -q)  # stops all running containers
-docker rm $(docker ps -aq)   # removes all containers
+docker stop brainrot-container
+docker rm brainrot-container
 
 # Remove old images
 docker rmi $(docker images -q ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/brainrot) || true
