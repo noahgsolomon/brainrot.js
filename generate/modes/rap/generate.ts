33d68562760fea37492bb268d4c11a7d771c5b2c
Noah Solomon <noahsolomon2003@gmail.com>
2025-03-03 14:43:27 -0500

    generate rap v0 for audio mode (video mode won't work rn)

--- generate/modes/rap/generate.ts
+++ generate/modes/rap/generate.ts
@@ -1,28 +1,29 @@
 import getAudioDuration from '../../audioDuration';
-import concatenateAudioFiles from '../../concat';
 import { generateCleanSrt } from '../../cleanSrt';
 import { query } from '../../dbClient';
 import { secondsToSrtTime, srtTimeToSeconds } from '../../transcribeAudio';
 import { transcribeAudio } from '../../transcribeAudio';
 import { generateFillerContext } from '../../fillerContext';
 import { writeFile } from 'fs/promises';
+import path from 'path';
+import ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
 
 export default async function generateRap({
 	local,
-	topic,
 	rapper,
 	lyrics,
 	audioUrl,
 	videoId,
+	outputType,
 }: {
 	local: boolean;
-	topic: string;
 	rapper: Rapper;
 	lyrics?: string;
 	audioUrl: string;
 	videoId?: string;
+	outputType: 'audio' | 'video';
 }) {
-	// fetch request to audio separator with the audio url, which returns a path to an instrumental file and a path to a vocal file
 	const { instrumentalPath, vocalPath } = await fetch(
 		'http://127.0.0.1:5555/audio-separator',
 		{
@@ -38,8 +39,39 @@ export default async function generateRap({
 
 	let startingTime = 0;
 
-	// Concatenate audio files if needed, or comment out if not used
-	concatenateAudioFiles();
+	// combine the instrumental and final vocal files into a single file audio.mp3
+	const combinedAudioPath = path.join('public', 'audio.mp3');
+
+	if (!fs.existsSync('tmp/')) {
+		fs.mkdirSync('tmp/');
+	}
+
+	// Combine instrumental and vocals using ffmpeg
+	await new Promise<void>((resolve, reject) => {
+		const command = ffmpeg();
+
+		command.input(instrumentalPath);
+
+		command.input(finalAudioPath);
+
+		command
+			.outputOptions([
+				'-filter_complex',
+				'[0:a][1:a]amix=inputs=2:duration=longest',
+			])
+			.on('start', (commandLine) => {
+				console.log('Spawned Ffmpeg with command:', commandLine);
+			})
+			.on('error', (err) => {
+				console.error('Error combining audio files:', err.message);
+				reject(err);
+			})
+			.on('end', () => {
+				console.log('Finished combining audio files!');
+				resolve();
+			})
+			.save(combinedAudioPath);
+	});
 
 	if (!local) {
 		await query(
@@ -48,77 +80,76 @@ export default async function generateRap({
 		);
 	}
 
-	// Perform transcription and get the result
-	const transcriptionResults = await transcribeAudio([vocalPath]);
+	if (outputType === 'video') {
+		const transcriptionResults = await transcribeAudio([vocalPath]);
 
-	const uncleanSrtContentArr = [];
+		const uncleanSrtContentArr = [];
 
-	// Iterate over each transcription result and corresponding audio file
-	for (let i = 0; i < (transcriptionResults ?? []).length; i++) {
-		const transcription = transcriptionResults![i][0];
-		let srtIndex = 1;
+		for (let i = 0; i < (transcriptionResults ?? []).length; i++) {
+			const transcription = transcriptionResults![i][0];
+			let srtIndex = 1;
 
-		let srtContent = '';
+			let srtContent = '';
 
-		const words = transcription.segments.flatMap(
-			(segment: any) => segment.words
-		);
-		for (let j = 0; j < words.length; j++) {
-			const word = words[j];
-			const nextWord = words[j + 1];
+			const words = transcription.segments.flatMap(
+				(segment: any) => segment.words
+			);
+			for (let j = 0; j < words.length; j++) {
+				const word = words[j];
+				const nextWord = words[j + 1];
 
-			const startTime = secondsToSrtTime(word.start);
+				const startTime = secondsToSrtTime(word.start);
 
-			const endTime = nextWord
-				? secondsToSrtTime(nextWord.start)
-				: secondsToSrtTime(word.end);
+				const endTime = nextWord
+					? secondsToSrtTime(nextWord.start)
+					: secondsToSrtTime(word.end);
 
-			srtContent += `${srtIndex}\n${startTime} --> ${endTime}\n${word.text}\n\n`;
-			srtIndex++;
+				srtContent += `${srtIndex}\n${startTime} --> ${endTime}\n${word.text}\n\n`;
+				srtIndex++;
+			}
+
+			const lines = srtContent.split('\n');
+
+			const incrementedSrtLines = lines.map((line) => {
+				const timeMatch = line.match(
+					/(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/
+				);
+				if (timeMatch) {
+					const startTime = srtTimeToSeconds(timeMatch[1]) + startingTime;
+					const endTime = srtTimeToSeconds(timeMatch[2]) + startingTime;
+					const incrementedStartTime = secondsToSrtTime(startTime);
+					const incrementedEndTime = secondsToSrtTime(endTime);
+					return `${incrementedStartTime} --> ${incrementedEndTime}`;
+				}
+				return line;
+			});
+
+			const incrementedSrtContent = incrementedSrtLines.join('\n');
+
+			const srtFileName = finalAudioPath
+				.replace('voice', 'srt')
+				.replace('.mp3', '.srt');
+
+			uncleanSrtContentArr.push({
+				content: incrementedSrtContent,
+				fileName: srtFileName,
+			});
+
+			const duration = await getAudioDuration(finalAudioPath);
+			startingTime += duration + 0.2;
+		}
+		if (!local) {
+			await query(
+				"UPDATE `pending-videos` SET status = 'Cleaning subtitle srt files', progress = 35 WHERE video_id = ?",
+				[videoId]
+			);
 		}
 
-		const lines = srtContent.split('\n');
+		if (lyrics) {
+			await generateCleanSrt([lyrics], uncleanSrtContentArr);
+		}
 
-		const incrementedSrtLines = lines.map((line) => {
-			const timeMatch = line.match(
-				/(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/
-			);
-			if (timeMatch) {
-				const startTime = srtTimeToSeconds(timeMatch[1]) + startingTime;
-				const endTime = srtTimeToSeconds(timeMatch[2]) + startingTime;
-				const incrementedStartTime = secondsToSrtTime(startTime);
-				const incrementedEndTime = secondsToSrtTime(endTime);
-				return `${incrementedStartTime} --> ${incrementedEndTime}`;
-			}
-			return line;
-		});
-
-		const incrementedSrtContent = incrementedSrtLines.join('\n');
-
-		const srtFileName = finalAudioPath
-			.replace('voice', 'srt')
-			.replace('.mp3', '.srt');
-
-		uncleanSrtContentArr.push({
-			content: incrementedSrtContent,
-			fileName: srtFileName,
-		});
-
-		const duration = await getAudioDuration(finalAudioPath);
-		startingTime += duration + 0.2;
-	}
-	if (!local) {
-		await query(
-			"UPDATE `pending-videos` SET status = 'Cleaning subtitle srt files', progress = 35 WHERE video_id = ?",
-			[videoId]
-		);
-	}
-
-	if (lyrics) {
-		await generateCleanSrt([lyrics], uncleanSrtContentArr);
-	}
-
-	let contextContent = `
+		let contextContent = `
 import { staticFile } from 'remotion';
 
 export const rapper: string = '${rapper}';
@@ -126,7 +157,8 @@ export const videoMode = 'rap';
 export const imageBackground: string = '/rap/${rapper}.png'
 `;
 
-	contextContent += generateFillerContext('rap');
+		contextContent += generateFillerContext('rap');
 
-	await writeFile('src/tmp/context.tsx', contextContent, 'utf-8');
+		await writeFile('src/tmp/context.tsx', contextContent, 'utf-8');
+	}
 }
